7cc4f040575f623a0f2345c107e85039414e492d
lid2000 <lid2000@gmail.com>
2018-02-07 17:12:03 +1100

    Added sample config. Should now delete entries that have been removed.

--- exportlj.js
+++ exportlj.js
@@ -11,12 +11,21 @@ db.serialize(function() {
 	db.run("CREATE TABLE IF NOT EXISTS `propmap` ( `id` INTEGER NOT NULL, `propname` NVARCHAR NOT NULL, PRIMARY KEY(`id`))");
 	db.run("CREATE TABLE IF NOT EXISTS `props` ( `itemid` INTEGER NOT NULL, `propid` INTEGER NOT NULL, `value` NVARCHAR NOT NULL)");
 	db.run("CREATE TABLE IF NOT EXISTS `general`( `lastsync` INTEGER)");
+	db.get("SELECT * FROM `general`", {}, function(err, res) {
+		if (!res) db.run("INSERT INTO `general` (`lastsync`) VALUES (0)");
+	});
 	db.run("CREATE TABLE IF NOT EXISTS `entries` ( `id` INTEGER NOT NULL, `subject` NVARCHAR, `event` TEXT NOT NULL, `time` INTEGER NOT NULL, `security` NVARCHAR NOT NULL, `allowmask` INTEGER, `anum` INTEGER NOT NULL, `url`	NVARCHAR NOT NULL, `poster` NVARCHAR, PRIMARY KEY(`id`))");
-	db.run("CREATE UNIQUE INDEX IF NOT EXISTS `id` ON `props` (`itemid`,	`propid`)");
+	db.run("CREATE UNIQUE INDEX IF NOT EXISTS `id` ON `props` (`itemid`, `propid`)");
 });
 
-const lj = new ljReq(url, username, password);
-lj.send({mode: "login", getmoods: 0, getpickws: 1, getpickwurls: 1}, function(data) {
-	const sync = new syncItems(db);
-	sync.sync();
+db.get("SELECT `lastsync` FROM `general`", {}, function(err, obj) {
+	exportLJ(obj ? obj['lastsync'] : 0);
 });
+
+const exportLJ = function(lastsync) {
+	const lj = new ljReq(url, username, password);
+	lj.send({mode: "login", getmoods: 0, getpickws: 1, getpickwurls: 1}, function(data) {
+		const sync = new syncItems(db, lastsync);
+		sync.sync();
+	});
+}
