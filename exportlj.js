69ebf43c4e4d11722704ed4fbdf83dec1130a60c
David Murray <dm@centreforaviation.com>
2018-02-07 17:32:50 +1100

    Added comments.

--- exportlj.js
+++ exportlj.js
@@ -6,8 +6,12 @@ const config = require('./config.json');
 
 const {url, username, password} = config;
 
+// a lot of this is done with callbacks because the database runs async, as does all the requests to the lj server.
+//	yeah, annoying.
+
 const db = new sqlite.Database(username + ".sqlite3");
 db.serialize(function() {
+	// set up the database
 	db.run("CREATE TABLE IF NOT EXISTS `propmap` ( `id` INTEGER NOT NULL, `propname` NVARCHAR NOT NULL, PRIMARY KEY(`id`))");
 	db.run("CREATE TABLE IF NOT EXISTS `props` ( `itemid` INTEGER NOT NULL, `propid` INTEGER NOT NULL, `value` NVARCHAR NOT NULL)");
 	db.run("CREATE TABLE IF NOT EXISTS `general`( `lastsync` INTEGER)");
@@ -19,12 +23,15 @@ db.serialize(function() {
 });
 
 db.get("SELECT `lastsync` FROM `general`", {}, function(err, obj) {
+	// we have our lastsync value, so let's get started
 	exportLJ(obj ? obj['lastsync'] : 0);
 });
 
 const exportLJ = function(lastsync) {
 	const lj = new ljReq(url, username, password);
 	lj.send({mode: "login", getmoods: 0, getpickws: 1, getpickwurls: 1}, function(data) {
+		// login was successful, so let's start syncing
+		// TODO: save moods, pics etc somewhere
 		const sync = new syncItems(db, lastsync);
 		sync.sync();
 	});
