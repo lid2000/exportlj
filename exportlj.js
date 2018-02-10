b53f8efe88b853c9a08b2ceeec1b8fe722604c42
David Murray <dm@centreforaviation.com>
2018-02-10 15:23:22 +1100

    Added props support.

--- exportlj.js
+++ exportlj.js
@@ -15,18 +15,18 @@ db.serialize(function() {
 	// set up the database
 	db.run("create table if not exists `commenters` ( `id` integer not null unique, `username` text not null, primary key(`id`) )");
 	db.run("create table if not exists `comments` ( `id` integer not null, `userid` integer not null, `entryid` integer not null, `parentid` integer not null, `date` integer not null, `subject` text not null, `comment` text not null )");
-	db.run("create table if not exists `props` ( `id` integer not null, `name` text not null, primary key(`id`))");
-	db.run("create table if not exists `propmap` ( `entryid` integer not null, `propid` integer not null, `value` text not null)");
+	db.run("create table if not exists `props` ( `entryid` integer not null, `name` text not null, `value` text not null)");
 	db.run("create table if not exists `general`( `lastsync` integer not null)");
 	db.get("select * from `general`", {}, function(err, res) {
 		if (!res) db.run("insert into `general` (`lastsync`) values (0)");
 	});
 	db.run("create table if not exists `entries` ( `id` integer not null, `subject` text, `event` text not null, `time` integer not null, `security` text not null, `allowmask` integer, `anum` integer not null, `url`	text not null, `poster` text, primary key(`id`))");
-	db.run("create unique index if not exists `id` on `propmap` (`entryid`, `propid`)");
+	db.run("create unique index if not exists `propindex` on `props` (`entryid`, `name`)");
 });
 
 db.get("select `lastsync` from `general`", {}, function(err, obj) {
 	// we have our lastsync value, so let's get started
+	console.log("Please note that this may take a while to complete after downloading due to general database thrashing (exportlj was written quickly, not efficiently).");
 	exportLJ(obj ? obj['lastsync'] : 0);
 });
 
