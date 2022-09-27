b39063f5a04f75a864e81c44d4ff6f2ac72ca70c
lid2000 <lid2000@gmail.com>
2022-09-27 15:34:02 +1000

    Download moods and userpic data.

--- exportlj.js
+++ exportlj.js
@@ -1,9 +1,10 @@
-const sqlite = require("sqlite3").verbose();
+import sqlite from "sqlite3";
 
-const ljReq = require("./lib/livejournal");
-const syncItems = require("./lib/syncitems");
-const getComments = require("./lib/comments");
-const config = require('./config.json');
+import ljReq from "./lib/livejournal.js";
+import syncItems from "./lib/syncitems.js";
+import getComments from "./lib/comments.js";
+import makeParams from "./lib/makeparams.js";
+import config from './config.json' assert { type: "json" };
 
 const {username} = config;
 
@@ -24,6 +25,9 @@ db.serialize(function() {
 	});
 	db.run("create table if not exists `entries` ( `id` integer not null, `subject` text, `event` text not null, `time` integer not null, `security` text not null, `allowmask` integer, `anum` integer not null, `url`	text not null, `poster` text, primary key(`id`))");
 	db.run("create index if not exists `entryindex` on `props` (`entryid`)");
+	db.run("create table if not exists `moods` (`id` integer not null, `parent` integer not null, `name` text not null, primary key(`id`))");
+	db.run("create table if not exists `userpics` (`name` text not null, `url` text not null, primary key(`name`))");
+	db.run("create unique index if not exists `propindex` on `props` (`entryid`, `name`)");
 });
 
 db.get("select `lastsync` from `general`", {}, function(err, obj) {
@@ -35,7 +39,25 @@ const exportLJ = function(lastsync) {
 	const lj = new ljReq();
 	lj.send({mode: "login", getmoods: 0, getpickws: 1, getpickwurls: 1}, function(data) {
 		// login was successful, so let's start syncing
-		// TODO: save moods, pics etc somewhere
+		// do moods
+		const addMood = db.prepare('insert or replace into moods (id, parent, name) values (:id, :parent, :name)');
+		for (let i = 1; i <= data.mood_count; i++) {
+			const mood = {
+				id: data['mood_' + i + '_id'],
+				parent: data['mood_' + i + '_parent'],
+				name: data['mood_' + i + '_name']
+			};
+			if (mood['id']) addMood.run(makeParams(mood));
+		}
+		const addUserpic = db.prepare('insert or replace into userpics (name, url) values (:name, :url)');
+		addUserpic.run(makeParams({name: "__default_pic__", url: data.defaultpicurl}));
+		for (let i = 1; i <= data.pickw_count; i++) {
+			const pic = {
+				name: data['pickw_' + i],
+				url: data['pickwurl_' + i]
+			};
+			addUserpic.run(makeParams(pic));
+		}
 		const sync = new syncItems(db, lastsync);
 		sync.sync();
 		db.get("select max(`id`) as maxid from comments", {}, function(err, obj) {
